ad09f701b0762cda29f43e3fe9419086e230b319
Donith001 <151123276+Donith001@users.noreply.github.com>
2023-11-24 00:45:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ import axios from 'axios'
 
 global.owner = [
   ['94767939688', 'Dinuwa Official²⁰²³', true],
-  ['94767939699', 'Dinuwa Official²⁰²³', true], 
-  ['94767939699'] 
+  ['94767939688', 'Dinuwa Official²⁰²³', true], 
+  [''] 
 ] //Number of owners
 
 global.mods = ['94767939688','94767939688'] 
