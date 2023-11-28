d6e32951378fce4167c66ac268aabd5ac130eefa
Donith001 <151123276+Donith001@users.noreply.github.com>
2023-11-28 12:51:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.botname = '💝 Queen Hentai 💝'
-global.premium = 'false'
+global.premium = 'true'
 global.packname = '💝 Queen Hentai 💝 Created By 💝 𝓡𝓪𝓼𝓱𝓲 𝓸𝓯𝓯𝓲𝓬𝓲𝓪𝓵   💝' 
 global.author = '💝 𝓡𝓪𝓼𝓱𝓲 𝓸𝓯𝓯𝓲𝓬𝓲𝓪𝓵   💝' 
 global.igfg = '▢ Follow on Facebook\https://www.facebook.com/profile.php?id=61551915615888\n' 
